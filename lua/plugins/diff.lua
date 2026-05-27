aed4d2744d91d88730dc62fac54c3698480b6532
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-05-27 12:04:52 +0530

    chore: adds plugin for code review

---
+++ lua/plugins/diff.lua
@@ -0,0 +1,19 @@
+return {
+  "afewyards/codereview.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  cmd = {
+    "CodeReview",
+    "CodeReviewAI",
+    "CodeReviewAIFile",
+    "CodeReviewStart",
+    "CodeReviewSubmit",
+    "CodeReviewApprove",
+    "CodeReviewOpen",
+    "CodeReviewPipeline",
+    "CodeReviewComments",
+    "CodeReviewFiles",
+    "CodeReviewToggleScroll",
+    "CodeReviewCommits",
+  },
+  opts = {},
+}
