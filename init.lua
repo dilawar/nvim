88c3058f6d24ec156a53c77f076701901069bffb
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-06-23 12:23:50 +0530

    if .make.sh found, use it

--- init.lua
+++ init.lua
@@ -1,2 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+--- local
+vim.api.nvim_create_autocmd("BufReadPre", {
+  pattern = "*",
+  callback = function()
+    local local_script = "./.make.sh"
+    if vim.fn.filereadable(local_script) == 1 then
+      -- % expands to the current file name, you can adjust the arguments
+      vim.opt_local.makeprg = local_script .. " %"
+    end
+  end,
+})
