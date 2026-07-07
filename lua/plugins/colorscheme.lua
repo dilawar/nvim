acb73644995f3b213db99f6f76606e62b64fe06f
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-07-07 14:02:15 +0530

    light gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
 return {
+  -- 1. Install the Gruvbox plugin
+  {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000, -- Make sure it loads first
+    config = true,
+  },
+  -- config lazyvim
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "delek",
+      colorscheme = "gruvbox",
     },
+    config = function(_, opts)
+      -- Force light mode before loading the theme
+      vim.o.background = "light"
+      require("lazyvim").setup(opts)
+    end,
   },
 }
