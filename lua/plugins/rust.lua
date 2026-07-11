1e3bc5472756a0223f8af81cc472a83d575cde8c
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-07-11 20:27:12 +0530

    chore: rust plugin for small repo

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,55 +1,9 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    opts = function()
-      vim.g.lazyvim_rust_diagnostics = "bacon-ls"
-    end,
-  },
-  {
-    "mrcjkb/rustaceanvim",
-    opts = {
-      server = {
-        on_attach = function(client, bufnr)
-          client.server_capabilities.semanticTokensProvider = nil
-        end,
-        default_settings = {
-          ["rust-analyzer"] = {
-            cargo = {
-              allFeatures = false,
-              loadOutDirsFromCheck = false,
-              buildScripts = {
-                enable = false,
-              },
-            },
-            checkOnSave = false,
-            diagnostics = {
-              enable = false,
-            },
-            files = {
-              excludeDirs = {
-                ".git",
-                "bazel-bin",
-                "bazel-out",
-                "bazel-testlogs",
-                "bazel-p21-embedded",
-                "target",
-                "node_modules",
-              },
-            },
-            procMacro = {
-              enable = false,
-            },
-          },
-        },
-      },
-    },
-  },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      inlay_hints = {
-        exclude = { "rust", "vue" },
-      },
-    },
-  },
+  'mrcjkb/rustaceanvim',
+  -- To avoid being surprised by breaking changes,
+  -- I recommend you set a version range
+  version = '^9',
+  -- This plugin implements proper lazy-loading (see :h lua-plugin-lazy).
+  -- No need for lazy.nvim to lazy-load it.
+  lazy = false,
 }
