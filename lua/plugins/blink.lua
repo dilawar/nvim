f9fd019ad5edc8fe9196655dffbb915494402459
Dilawar Singh <dilawar.s.rajput@gmail.com>
2026-07-26 12:52:34 +0530

    chore: cleanup in auto-suggestions

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,20 +1,33 @@
 return {
   "saghen/blink.cmp",
   opts = {
+    -- Root configuration fallback
+    show_on_keyword = false,
+    show_on_trigger_character = false,
+
+    -- Nested block configuration fallback
     completion = {
-      -- Disable automatic trigger
       trigger = {
         prefetch_on_insert = false,
         show_on_keyword = false,
         show_on_trigger_character = false,
       },
-      -- Show documentation only when manually triggered
+      menu = { auto_show = false },
+      ghost_text = { enabled = false },
       documentation = { auto_show = false },
     },
+
     keymap = {
       preset = "default",
-      -- Use Ctrl + Space to trigger completion manually
       ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+    },
+
+    sources = {
+      providers = {
+        snippets = {
+          min_keyword_length = 0,
+        },
+      },
     },
   },
 }
