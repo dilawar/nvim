8826fc21f0f91920e47a1381025d3e445f408475
Dilawar Singh <dilawar.s@toyotaconnected.co.in>
2026-08-08 14:39:30 +0530

    chore: get tcin blink settings

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,26 +1,10 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    -- Nested block configuration fallback
-    completion = {
-      trigger = {
-        prefetch_on_insert = false,
-        show_on_keyword = false,
-        show_on_trigger_character = false,
-      },
-      menu = { auto_show = false },
-      ghost_text = { enabled = false },
-      documentation = { auto_show = false },
-    },
     keymap = {
-      preset = "super-tab",
-    },
-    sources = {
-      providers = {
-        snippets = {
-          min_keyword_length = 0,
-        },
-      },
+      preset = "enter",
+      ["<S-Tab>"] = { "select_prev", "fallback" },
+      ["<Tab>"] = { "select_next", "fallback" },
     },
   },
 }
