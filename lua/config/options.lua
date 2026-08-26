8b3e7ea9c5a84774eeb78b0ee3ecb1858cfcb351
Dilawar Singh <dilawar.s@toyotaconnected.co.in>
2026-08-26 09:14:33 +0530

    chore: blinks use super-tab

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -26,13 +26,5 @@ if vim.fn.executable("nvr") == 1 then
   vim.env.VISUAL = nvr .. "-l --remote" -- (Optional)
 end
 
--- Enable patchmode to generate a .orig backup file on the first save
-vim.opt.patchmode = ".orig"
-vim.opt.backupdir = vim.fn.stdpath("data") .. "/backup//"
-local backup_dir = vim.fn.expand(vim.opt.backupdir._value:match("(.*)//$") or vim.opt.backupdir._value)
-if vim.fn.isdirectory(backup_dir) == 0 then
-  vim.fn.mkdir(backup_dir, "p")
-end
-
 -- env
 vim.env.GOROOT = "/opt/homebrew/Cellar/go/1.26.5/libexec"
